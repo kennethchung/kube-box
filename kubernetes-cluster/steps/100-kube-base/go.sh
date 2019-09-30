dfafea59026f57af059261c95cc11e97066028b8
kchung <k.chung@f5.com>
2019-09-29 21:28:37 -0700

    added addon installation

--- kubernetes-cluster/steps/100-kube-base/go.sh
+++ kubernetes-cluster/steps/100-kube-base/go.sh
@@ -14,6 +14,7 @@ KUBE_ADVERTISE_ADDRESS="$(hostname -I | awk '{print $1}')"
 export KUBE_ADVERTISE_ADDRESS
 export KUBE_ALLOW_PRIVILEGED=true
 export KUBE_CLUSTER_CIDR=10.244.0.0/16
+export HOSTNAME=$(hostname)
 envsubst <"./kube-config-template.yaml" > ./kube-config.yaml
 bash -c "cat <<EOF >>./kube-config.yaml
     - $(hostname)
@@ -26,5 +27,12 @@ EOF"
     done
 source ./"${os_script}"
 sudo kubeadm init --config ./kube-config.yaml
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+printgreen "Installing addon flannel"
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml        
+printgreen "Untaint master node so that pods can be scheduled master node"
+kubectl taint nodes ${HOSTNAME} node-role.kubernetes.io/master-
 printgreen "Completed installing kubernetes"
 source ./inspect.sh
