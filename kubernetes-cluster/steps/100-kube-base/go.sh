ceba3c199853d914e74e58ca48c8982d755d2906
kchung <k.chung@f5.com>
2019-09-30 13:58:08 -0700

    refactored the template

--- kubernetes-cluster/steps/100-kube-base/go.sh
+++ kubernetes-cluster/steps/100-kube-base/go.sh
@@ -13,24 +13,23 @@ printgreen "Installing kubernetes......."
 KUBE_ADVERTISE_ADDRESS="$(hostname -I | awk '{print $1}')"
 export KUBE_ADVERTISE_ADDRESS="${KUBE_ADVERTISE_ADDRESS}"
 export KUBE_ALLOW_PRIVILEGED=true
+# For flannel to work correctly, you must pass --pod-network-cidr=10.244.0.0/16 to kubeadm init.
 export KUBE_CLUSTER_CIDR=10.244.0.0/16
+export KUBE_CLUSTER_DOMAIN=cluster.local
+export KUBE_SERVICE_ADDRESSES=10.96.0.0/12
 export HOSTNAME=$(hostname)
 envsubst <"./kube-config-template.yaml" > ./kube-config.yaml
 read -r -a IPS <<<"$(hostname -I)"
 bash -c "cat <<EOF >>./kube-config.yaml
-- ${HOSTNAME}
+    - ${HOSTNAME}
 EOF"
-
 for ip in "${IPS[@]}"; do
-    # adds ips
 bash -c "cat <<EOF >>./kube-config.yaml
     - ${ip}
 EOF"
 done
-
-source ./"${os_script}"
-
 cat ./kube-config.yaml
+source ./"${os_script}"
 sudo kubeadm init --config ./kube-config.yaml
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
